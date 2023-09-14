3bbd6a207ba686cba19d937968aa70ef0e0b7305
emerson <emersonrox8@gmail.com>
2023-09-13 23:58:57 -0300

    Adcionado o modulo que pega os dados da coinbase

---
+++ credentials.py
@@ -0,0 +1,26 @@
+from google.oauth2 import service_account
+import pandas_gbq
+import json
+
+#var constante
+key_path = "/home/emerson/codes/python/Dados/projetos_etl/documento/emersondai254-dd3a10e2ae65.json"
+
+#Cria a conexão com o bigquery, usando a service account json
+def credentials():
+    credentials = service_account.Credentials.from_service_account_file(key_path)
+    
+    return credentials
+
+#Vai até o arquivo do service account e recupera o nome do projeto, para não deixar hard code
+def get_project():
+    with open(key_path, 'r') as file:
+        project_id = json.load(file)
+
+    project_id = project_id['project_id']
+
+    return project_id
+
+#Cria a conexão, usando a biblioteca pandas_gbq
+def creating_context_gcp():
+    pandas_gbq.context.credentials = credentials()
+    pandas_gbq.context.project = get_project
---
+++ etl.py
@@ -0,0 +1,10 @@
+import credentials
+
+def data_extract():
+    pass
+
+def data_transform():
+    pass
+
+def data_load():
+    pass
